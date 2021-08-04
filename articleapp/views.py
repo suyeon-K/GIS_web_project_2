e018c07a855081215c1077362b1f3e0f6f0778ea
suyeon-K <74526895+suyeon-K@users.noreply.github.com>
2021-08-04 16:11:02 +0900

    decorator

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,13 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
 
 
+@method_decorator(login_required, 'get')  # 장고에서 제공하는 로그인 여부를 확인하는 함수
+@method_decorator(login_required, 'post')
 class ArticleCreateView(CreateView):
     model = Article
     form_class = ArticleCreationForm
@@ -28,6 +33,8 @@ class ArticleDetailView(DetailView):
     template_name = 'articleapp/detail.html'
 
 
+@method_decorator(article_ownership_required,'get')
+@method_decorator(article_ownership_required,'post')
 class ArticleUpdateView(UpdateView):
     model = Article
     form_class = ArticleCreationForm
@@ -35,9 +42,11 @@ class ArticleUpdateView(UpdateView):
     template_name = 'articleapp/update.html'
 
     def get_success_url(self):
-        return reverse('articleapp:detail', kwargs={'pk':self.object.pk})
+        return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
 
 
+@method_decorator(article_ownership_required,'get')
+@method_decorator(article_ownership_required,'post')
 class ArticleDeleteView(DeleteView):
     model = Article
     context_object_name = 'target_article'
